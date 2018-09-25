78cc318a7121099a71b1108473896fb8c3f15d6c
Jason Brewer <jason.brewer@arup.com>
2018-09-25 16:21:18 +0100

    Flask now serves the Angular app, ng build outputs into the Flask 'static' folder

--- mw_serve/flask/omx-player-service.py
+++ mw_serve/flask/omx-player-service.py
@@ -1,11 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory, render_template
 from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
+import os
 # from omxplayer.player import OMXPlayer
 
-app = Flask(__name__)
+app = Flask(__name__,  static_folder='static')
 api = Api(app)
 
 CORS(app)
@@ -14,9 +15,15 @@ AUDIO_PATH_MLP = "/opt/02_Planets_Part1_Treatment.mlp"
 AUDIO_PATH_TEST = "/opt/demo_5ch/ChID-BLITS-EBU-Narration441-16b.wav"
 # player = OMXPlayer(AUDIO_PATH_MLP, args=['--layout', '5.1', '-w', '-o', 'hdmi'])
 
-@app.route("/")
-def hello():
-    return jsonify({'text':'World, Hello?'})  
+# serve the angular app
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists("static/" + path):
+        return send_from_directory('static/', path)
+    else:
+        return send_from_directory('static/', 'index.html')
 
 class GetTrackList(Resource):
   def get(self):
